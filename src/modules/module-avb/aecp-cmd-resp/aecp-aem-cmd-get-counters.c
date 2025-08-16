169eebf9d8a553a2ff1bccfa58f8e1e3de02798e
hackerman-kl <hackerman-kl@kebag-logic.com>
2025-08-16 14:10:55 +0000

    #77: adapting the counters to work with new refactoring

--- src/modules/module-avb/aecp-cmd-resp/aecp-aem-cmd-get-counters.c
+++ src/modules/module-avb/aecp-cmd-resp/aecp-aem-cmd-get-counters.c
@@ -17,18 +17,16 @@
 #include "aecp-aem-unsol-helper.h"
 
 static int handle_get_counters_avb_interface(struct aecp *aecp, uint8_t *buf,
-    uint16_t desc_id)
+    struct descriptor *desc)
 {
-    struct aecp_aem_counter_avb_interface_state if_state = {0};
+    struct aecp_aem_avb_interface_state *if_st =
+             (struct aecp_aem_avb_interface_state *) desc->ptr;
+
+    struct aecp_aem_counter_avb_interface_state *if_state =
+             (struct aecp_aem_counter_avb_interface_state *) &if_st->counters;
     uint32_t *counters = (uint32_t*) buf;
-    int rc;
+    int rc = 0;
 
-    rc = aecp_aem_get_state_var(aecp, aecp->server->entity_id ,
-         aecp_aem_counter_avb_interface, desc_id, &if_state);
-
-    if (rc) {
-        spa_assert(0);
-    }
 
     unaligned_copy_u32(counters, htonl(
         AECP_AEM_COUNTER_GET_MASK(AECP_AEM_COUNTER_AVB_IF_LINK_UP) |
@@ -41,39 +39,37 @@ static int handle_get_counters_avb_interface(struct aecp *aecp, uint8_t *buf,
 
     counters++;
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_AVB_IF_LINK_UP,
-         htonl(if_state.link_up));
+         htonl(if_state->link_up));
 
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_AVB_IF_LINK_DOWN,
-        htonl(if_state.link_down));
+        htonl(if_state->link_down));
 
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_AVB_IF_GPTP_GM_CH,
-        htonl(if_state.gptp_gm_changed));
+        htonl(if_state->gptp_gm_changed));
 
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_AVB_IF_FRAME_TX,
-        htonl(if_state.frame_tx));
+        htonl(if_state->frame_tx));
 
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_AVB_IF_FRAME_RX,
-        htonl(if_state.frame_rx));
+        htonl(if_state->frame_rx));
 
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_AVB_IF_RX_CRC_ERROR,
-            htonl(if_state.error_crc));
+            htonl(if_state->error_crc));
 
     return rc;
 }
 
 static int handle_get_counters_clock_domain(struct aecp *aecp, uint8_t *buf,
-    uint16_t desc_id)
+    struct descriptor *desc)
 {
-    struct aecp_aem_counter_clock_domain_state cd_state = {0};
+    struct aecp_aem_clock_domain_state *cd =
+        (struct aecp_aem_clock_domain_state *) desc->ptr;
+
+    struct aecp_aem_counter_clock_domain_state *cd_state =
+         (struct aecp_aem_counter_clock_domain_state *) &cd->counters;
+
     uint32_t *counters = (uint32_t*) buf;
-    int rc;
-
-    rc = aecp_aem_get_state_var(aecp, aecp->server->entity_id ,
-            aecp_aem_counter_clock_domain, desc_id, &cd_state);
-
-    if (rc) {
-        spa_assert(0);
-    }
+    int rc = 0;
 
     unaligned_copy_u32(counters, htonl(
         AECP_AEM_COUNTER_GET_MASK(AECP_AEM_COUNTER_CLK_DOMAIN_LOCKED) |
@@ -82,27 +78,26 @@ static int handle_get_counters_clock_domain(struct aecp *aecp, uint8_t *buf,
 
     counters++;
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_CLK_DOMAIN_LOCKED,
-        htonl(cd_state.locked));
+        htonl(cd_state->locked));
 
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_CLK_DOMAIN_UNLOCKED,
-        htonl(cd_state.unlocked));
+        htonl(cd_state->unlocked));
 
     return rc;
 }
 
 static int handle_get_counters_stream_input(struct aecp *aecp, uint8_t *buf,
-    uint16_t desc_id)
+    struct descriptor *desc)
 {
-    struct aecp_aem_counter_stream_input_state si_state = {0};
+    struct aecp_aem_stream_input_state *si =
+        (struct aecp_aem_stream_input_state *) desc->ptr;
+
+    struct aecp_aem_counter_stream_input_state *si_state =
+        (struct aecp_aem_counter_stream_input_state *) &si->counters;
+
     uint32_t *counters = (uint32_t*) buf;
-    int rc;
+    int rc = 0;
 
-    rc = aecp_aem_get_state_var(aecp, aecp->server->entity_id ,
-            aecp_aem_counter_stream_input, desc_id, &si_state);
-
-    if (rc) {
-        spa_assert(0);
-    }
 
     unaligned_copy_u32(counters, htonl(
         AECP_AEM_COUNTER_GET_MASK(AECP_AEM_COUNTER_STREAM_INPUT_MEDIA_LOCKED) |
@@ -119,51 +114,49 @@ static int handle_get_counters_stream_input(struct aecp *aecp, uint8_t *buf,
 
     counters++;
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_STREAM_INPUT_MEDIA_LOCKED,
-        htonl(si_state.media_locked));
+        htonl(si_state->media_locked));
 
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_STREAM_INPUT_MEDIA_UNLOCKED,
-        htonl(si_state.media_unlocked));
+        htonl(si_state->media_unlocked));
 
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_STREAM_INPUT_STREAM_INTERRUPTED,
-        htonl(si_state.stream_interrupted));
+        htonl(si_state->stream_interrupted));
 
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_STREAM_INPUT_SEQ_NUM_MISMATCH,
-        htonl(si_state.seq_mistmatch));
+        htonl(si_state->seq_mistmatch));
 
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_STREAM_INPUT_MEDIA_RESET,
-        htonl(si_state.media_reset));
+        htonl(si_state->media_reset));
 
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_STREAM_INPUT_TIMESTAMP_UNCERTAIN,
-        htonl(si_state.tu));
+        htonl(si_state->tu));
 
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_STREAM_INPUT_UNSUPPORTED_FORMAT,
-        htonl(si_state.unsupported_format));
+        htonl(si_state->unsupported_format));
 
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_STREAM_INPUT_LATE_TIMESTAMP,
-        htonl(si_state.late_timestamp));
+        htonl(si_state->late_timestamp));
 
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_STREAM_INPUT_EARLY_TIMESTAMP,
-        htonl(si_state.early_timestamp));
+        htonl(si_state->early_timestamp));
 
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_STREAM_INPUT_FRAME_RX,
-        htonl(si_state.frame_rx));
+        htonl(si_state->frame_rx));
 
     return rc;
 }
 
 static int handle_get_counters_stream_output(struct aecp *aecp, uint8_t *buf,
-    uint16_t desc_id)
+    struct descriptor *desc)
 {
-    struct aecp_aem_counter_stream_output_state so_state = {0};
+    struct aecp_aem_stream_output_state *so =
+        (struct aecp_aem_stream_output_state *) desc->ptr;
+
+    struct aecp_aem_counter_stream_output_state *so_state =
+        (struct aecp_aem_counter_stream_output_state *) &so->counters;
+
     uint32_t *counters = (uint32_t*) buf;
-    int rc;
-
-    rc = aecp_aem_get_state_var(aecp, aecp->server->entity_id ,
-            aecp_aem_counter_stream_output, desc_id, &so_state);
-
-    if (rc) {
-        spa_assert(0);
-    }
+    int rc = 0;
 
     unaligned_copy_u32(counters, htonl(
         AECP_AEM_COUNTER_GET_MASK(AECP_AEM_COUNTER_STREAM_OUT_STREAM_START) |
@@ -176,44 +169,44 @@ static int handle_get_counters_stream_output(struct aecp *aecp, uint8_t *buf,
     counters++;
 
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_STREAM_OUT_STREAM_START,
-        htonl(so_state.stream_start));
+        htonl(so_state->stream_start));
 
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_STREAM_OUT_STREAM_STOP,
-        htonl(so_state.stream_stop));
+        htonl(so_state->stream_stop));
 
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_STREAM_OUT_FRAME_TX,
-        htonl(so_state.frame_tx));
+        htonl(so_state->frame_tx));
 
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_STREAM_OUT_TIMESTAMP_UNCERTAIN,
-        htonl(so_state.tu));
+        htonl(so_state->tu));
 
     unaligned_copy_u32(counters + AECP_AEM_COUNTER_STREAM_OUT_MEDIA_RESET,
-        htonl(so_state.media_reset));
+        htonl(so_state->media_reset));
 
     return rc;
 }
 
-static int fill_counters_and_validity_bits(struct aecp *aecp, uint8_t *buf, uint16_t desc_id,
-        uint16_t desc_type)
+static int fill_counters_and_validity_bits(struct aecp *aecp, uint8_t *buf,
+    struct descriptor *desc, uint16_t desc_type)
 {
     int rc = 0;
     switch (desc_type) {
         case AVB_AEM_DESC_AVB_INTERFACE:
-            rc = handle_get_counters_avb_interface(aecp, buf, desc_id);
+            rc = handle_get_counters_avb_interface(aecp, buf, desc);
         break;
         case AVB_AEM_DESC_CLOCK_DOMAIN:
-            rc = handle_get_counters_clock_domain(aecp, buf, desc_id);
+            rc = handle_get_counters_clock_domain(aecp, buf, desc);
 
         break;
         case AVB_AEM_DESC_STREAM_INPUT:
-            rc = handle_get_counters_stream_input(aecp, buf, desc_id);
+            rc = handle_get_counters_stream_input(aecp, buf, desc);
         break;
         case AVB_AEM_DESC_STREAM_OUTPUT:
-            rc = handle_get_counters_stream_output(aecp, buf, desc_id);
+            rc = handle_get_counters_stream_output(aecp, buf, desc);
         break;
         default:
-        pw_log_warn("not suppoorted get Counter for desc id %d type %d\n",
-                        desc_id, desc_type);
+        pw_log_warn("not suppoorted get Counter for desc type %d\n",
+                     desc_type);
         // All validity bit set to zero
             memset(buf, 0, sizeof(uint32_t));
         break;
@@ -223,26 +216,18 @@ static int fill_counters_and_validity_bits(struct aecp *aecp, uint8_t *buf, uint
 }
 
 int prepare_get_counter_packet(struct aecp *aecp, uint8_t *buf, int *len,
-    uint16_t desc_index, uint16_t desc_type, bool is_unsolicited)
+    struct descriptor *desc, uint16_t desc_type, bool is_unsolicited)
 {
     /** reply */
     struct avb_ethernet_header *h_reply = (struct avb_ethernet_header *)buf;
     struct avb_packet_aecp_aem *p_reply = SPA_PTROFF(h_reply, sizeof(*h_reply), void);
     struct avb_packet_aecp_aem_get_counters_resp *g_counters_r;
     uint16_t ctrl_data_length;
-
     int rc;
 
    g_counters_r = (struct avb_packet_aecp_aem_get_counters_resp*)p_reply->payload;
-
    rc = fill_counters_and_validity_bits(aecp, (uint8_t*) &g_counters_r->counter_valid,
-                                             desc_index, desc_type);
-
-    if (rc) {
-        pw_log_error("Error while handling counters for desc id %d type %d",
-                     desc_index, desc_type);
-        spa_assert(0);
-    }
+                                             desc, desc_type);
 
     AVB_PACKET_AEM_SET_COMMAND_TYPE(p_reply, AVB_AECP_AEM_CMD_GET_COUNTERS);
     *len = sizeof(*g_counters_r) + sizeof(*p_reply) + sizeof(*h_reply);
@@ -257,16 +242,25 @@ int prepare_get_counter_packet(struct aecp *aecp, uint8_t *buf, int *len,
         AVB_PACKET_SET_LENGTH(&p_reply->aecp.hdr, ctrl_data_length);
     }
 
-    return 0;
+    return rc;
 }
 
 static int do_unsolicited_get_counters(struct aecp *aecp, uint8_t *buf,
     uint16_t desc_type, uint16_t desc_id)
 {
-    struct aecp_aem_counter_avb_interface_state avb_if = {0};
-    struct aecp_aem_counter_clock_domain_state cd_state = {0};
-    struct aecp_aem_counter_stream_input_state si_state = {0};
-    struct aecp_aem_counter_stream_output_state so_state = {0};
+    // Future, using the max address, identify from where this packet is coming from
+    struct aecp_aem_avb_interface_state *avb;
+    struct aecp_aem_clock_domain_state *cd;
+    struct aecp_aem_stream_input_state *sie;
+    struct aecp_aem_stream_output_state *so;
+
+    // To keep old behavior, change as less a possible, then fix it later
+    struct aecp_aem_counter_avb_interface_state *avb_if;
+    struct aecp_aem_counter_clock_domain_state *cd_state;
+    struct aecp_aem_counter_stream_input_state *si_state;
+    struct aecp_aem_counter_stream_output_state *so_state;
+
+    struct descriptor *desc;
     struct aecp_aem_base_info *binfo;
 
     int64_t update_time;
@@ -274,57 +268,45 @@ static int do_unsolicited_get_counters(struct aecp *aecp, uint8_t *buf,
     int rc;
     int len;
 
+    desc = server_find_descriptor(aecp->server, desc_type, desc_id);
+    if (!desc) {
+        pw_log_warn("Descriptor %u, %u not found \n", desc_type, desc_id);
+        return -1;
+    }
+
     switch (desc_type) {
         case AVB_AEM_DESC_AVB_INTERFACE:
-            rc = aecp_aem_get_state_var(aecp, aecp->server->entity_id ,
-                aecp_aem_counter_avb_interface, desc_id, &avb_if);
 
-            if (rc) {
-                spa_assert(0);
-            }
-
-            update_time = avb_if.base_desc.base_info.expire_timeout;
-            needs_update = avb_if.base_desc.base_info.needs_update;
-            binfo = &avb_if.base_desc.base_info;
+            avb = (struct aecp_aem_avb_interface_state *) desc->ptr;
+            avb_if = (struct aecp_aem_counter_avb_interface_state *) &avb->counters;
+            update_time = avb_if->base_desc.base_info.expire_timeout;
+            needs_update = avb_if->base_desc.base_info.needs_update;
+            binfo = &avb_if->base_desc.base_info;
         break;
         case AVB_AEM_DESC_CLOCK_DOMAIN:
 
-            rc = aecp_aem_get_state_var(aecp, aecp->server->entity_id ,
-                    aecp_aem_counter_clock_domain, desc_id, &cd_state);
-
-            if (rc) {
-                spa_assert(0);
-            }
-
-            update_time = cd_state.base_desc.base_info.expire_timeout;
-            needs_update = cd_state.base_desc.base_info.needs_update;
-            binfo = &cd_state.base_desc.base_info;
+            cd = (struct aecp_aem_clock_domain_state *) desc->ptr;
+            cd_state = (struct aecp_aem_counter_clock_domain_state *) &cd->counters;
+            update_time = cd_state->base_desc.base_info.expire_timeout;
+            needs_update = cd_state->base_desc.base_info.needs_update;
+            binfo = &cd_state->base_desc.base_info;
         break;
         case AVB_AEM_DESC_STREAM_INPUT:
+            sie = (struct aecp_aem_stream_input_state *) desc->ptr;
+            si_state = (struct aecp_aem_counter_stream_input_state *)&sie->counters;
 
-            rc = aecp_aem_get_state_var(aecp, aecp->server->entity_id ,
-                    aecp_aem_counter_stream_input, desc_id, &si_state);
-
-            if (rc) {
-                spa_assert(0);
-            }
-
-            update_time = si_state.base_desc.base_info.expire_timeout;
-            needs_update = si_state.base_desc.base_info.needs_update;
-            binfo = &si_state.base_desc.base_info;
+            update_time = si_state->base_desc.base_info.expire_timeout;
+            needs_update = si_state->base_desc.base_info.needs_update;
+            binfo = &si_state->base_desc.base_info;
         break;
         case AVB_AEM_DESC_STREAM_OUTPUT:
 
-            rc = aecp_aem_get_state_var(aecp, aecp->server->entity_id ,
-                    aecp_aem_counter_stream_output, desc_id, &so_state);
+            so = (struct aecp_aem_stream_output_state *) desc->ptr;
+            so_state = (struct aecp_aem_counter_stream_output_state *) &so->counters;
 
-            if (rc) {
-                spa_assert(0);
-            }
-
-            update_time = so_state.base_desc.base_info.expire_timeout;
-            needs_update = so_state.base_desc.base_info.needs_update;
-            binfo = &so_state.base_desc.base_info;
+            update_time = so_state->base_desc.base_info.expire_timeout;
+            needs_update = so_state->base_desc.base_info.needs_update;
+            binfo = &so_state->base_desc.base_info;
         break;
         default:
             pw_log_error("not suppoorted get Counter for desc id %d type %d\n",
@@ -337,7 +319,7 @@ static int do_unsolicited_get_counters(struct aecp *aecp, uint8_t *buf,
         return 0;
     }
 
-    rc = prepare_get_counter_packet(aecp, buf, &len, desc_id, desc_type, true);
+    rc = prepare_get_counter_packet(aecp, buf, &len, desc, desc_type, true);
     if (rc) {
         spa_assert(0);
     }
@@ -346,36 +328,9 @@ static int do_unsolicited_get_counters(struct aecp *aecp, uint8_t *buf,
     if (rc) {
         spa_assert(0);
     }
-    switch (desc_type) {
-        case AVB_AEM_DESC_AVB_INTERFACE:
-            avb_if.base_desc.base_info.expire_timeout += SPA_NSEC_PER_MSEC;
-            avb_if.base_desc.base_info.needs_update = false;
-            rc = aecp_aem_refresh_state_var(aecp, aecp->server->entity_id,
-                        aecp_aem_counter_avb_interface, desc_id, &avb_if);
-        break;
-        case AVB_AEM_DESC_CLOCK_DOMAIN:
-            cd_state.base_desc.base_info.expire_timeout += SPA_NSEC_PER_SEC;
-            cd_state.base_desc.base_info.needs_update = false;
-            rc = aecp_aem_refresh_state_var(aecp, aecp->server->entity_id,
-                aecp_aem_counter_clock_domain, desc_id, &cd_state);
-        break;
-        case AVB_AEM_DESC_STREAM_INPUT:
-            si_state.base_desc.base_info.expire_timeout += SPA_NSEC_PER_SEC;
-            si_state.base_desc.base_info.needs_update = false;
-            rc = aecp_aem_refresh_state_var(aecp, aecp->server->entity_id,
-                aecp_aem_counter_stream_input, desc_id, &si_state);
-        break;
-        case AVB_AEM_DESC_STREAM_OUTPUT:
-            so_state.base_desc.base_info.expire_timeout += SPA_NSEC_PER_SEC;
-            so_state.base_desc.base_info.needs_update = false;
-            rc = aecp_aem_refresh_state_var(aecp, aecp->server->entity_id,
-                aecp_aem_counter_stream_output, desc_id, &so_state);
-        break;
-        default:
-            pw_log_error("not suppoorted get Counter for desc id %d type %d\n",
-                            desc_id, desc_type);
-            spa_assert(0);
-    }
+
+    binfo->expire_timeout += SPA_NSEC_PER_MSEC;
+    binfo->needs_update += SPA_NSEC_PER_MSEC;
 
     return rc;
 }
@@ -409,7 +364,7 @@ int handle_cmd_get_counters(struct aecp *aecp, int64_t now, const void *m,
     memset(buf, 0, sizeof(buf));
     memcpy(buf, m, len);
 
-    rc = prepare_get_counter_packet(aecp, buf, &len, desc_index, desc_type, false);
+    rc = prepare_get_counter_packet(aecp, buf, &len, desc, desc_type, false);
     if (rc) {
         spa_assert(0);
     }
